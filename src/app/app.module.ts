fe868c057404874a0efb1b83ae39da93a836e6eb
Roman Degterov <wize.arbik@gmail.com>
2022-06-09 15:36:02 +0300

    feat: add custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TreeComponent } from './components/tree/tree.component';
 import { SearchDirective } from './directives/search.directive';
+import { FormatPipe } from './pipes/format.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent, TreeComponent, SearchDirective
+    AppComponent, TreeComponent, SearchDirective, FormatPipe
   ],
   imports: [
     BrowserModule, ReactiveFormsModule, FormsModule
---
+++ src/app/pipes/format.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+  
+@Pipe({
+    name: 'format'
+})
+export class FormatPipe implements PipeTransform {
+  transform(value: any): any {
+    if(!value) {
+      if(typeof value === 'string') return 'Имя не определено!';
+      if(typeof value === 'number') return 'Возраст не определен!';
+    } else {
+      return value;
+    }
+  }
+}
